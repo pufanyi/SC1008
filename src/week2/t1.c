75eabc7625a21b9baa44babacee85be5031d3eea
pufanyi <pufanyi@sensetime.com>
2026-01-16 16:04:20 +0800

    feat: calculate and display solutions for a system of two linear equations.

--- src/week2/t1.c
+++ src/week2/t1.c
@@ -4,5 +4,8 @@ int main() {
   double a1, b1, c1, a2, b2, c2;
   printf("Enter the values for a1, b1, c1, a2, b2, c2:\n");
   scanf("%lf%lf%lf%lf%lf%lf", &a1, &b1, &c1, &a2, &b2, &c2);
+  double x = (b2 * c1 - b1 * c2) / (a1 * b2 - a2 * b1);
+  double y = (a1 * c2 - a2 * c1) / (a1 * b2 - a2 * b1);
+  printf("x = %.2f and y = %.2f\n", x, y);
   return 0;
 }
